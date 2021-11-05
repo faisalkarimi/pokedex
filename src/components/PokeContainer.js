9c2458d801820ccd4971245922654aca0fba8ff4
ahfaisalkarimi <ahfaisalkarimi@gmail.com>
2021-11-04 22:30:48 -0700

    Initial Pokedex setup

---
+++ src/components/PokeContainer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Header'
+
+const PokeContainer = () => {
+    return (
+        <>
+        <Header />        
+        <div className="poke-container" id="poke-container">
+            
+        </div>
+        </>
+    )
+}
+
+export default PokeContainer
